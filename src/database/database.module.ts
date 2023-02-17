fecf856df29a52472f097b3503cfdd2217ae91bb
David Barrios <david@hotelmansa.com>
2023-02-16 22:21:00 -0300

    add db

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -7,10 +7,10 @@ import User from '../user/entity/user.entity';
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
+      host: 'ls-f9f2c60434ab2e2f9ecb9c1ff6b14f6f7f4bb1eb.cb9l0bk24fup.us-east-1.rds.amazonaws.com',
       port: 3306,
-      username: 'root',
-      password: 'secret',
+      username: 'dbmasteruser',
+      password: 'F-,f_)v;i<M{F,2[8zDczHd:N!4Fgs^Z',
       database: 'nest',
       entities: [User],
       synchronize: true,
